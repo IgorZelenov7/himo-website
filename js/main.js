902ed57cd0957c161040da8d2c758e1547222ac3
Igor Zelenov <izelenov7@mail.ru>
2025-02-02 01:39:07 +0300

    add section #2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ $(function(){
 
     $('.bike-slider').slick({
         arrows: false,
-        dots: true
+        dots: true,
+        fade: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
 
 });
